077c47920e6527879dce877858b14e3ebdda9b50
chase-vgo <168204519+chase-vgo@users.noreply.github.com>
2024-07-11 08:51:33 -0500

    Righted wrongs

--- CIPPAPIModule/public/CIPP/Core/Get-CIPPLogs.ps1
+++ CIPPAPIModule/public/CIPP/Core/Get-CIPPLogs.ps1
@@ -5,21 +5,52 @@ Retrieves CIPP logs from the API.
 .DESCRIPTION
 The Get-CIPPLogs function retrieves logs from the CIPP API by invoking the "/api/ListLogs" endpoint.
 
-.PARAMETER None
-This function does not accept any parameters.
+.PARAMETER Severity
+Accepts any of: debug,info,warn,error,critical,alert. If DateFilter is not specified alongside, it assumes current date in local time.
+
+.PARAMETER DateFilter
+Date in "yyyyMMdd" format. This should be in the time zone of your CIPP instance (default UTC).
 
 .EXAMPLE
 Get-CIPPLogs
 Retrieves CIPP logs from the API.
 
+Get-CIPPLogs -Severity "Alert" -DateFilter "20240711"
+Retrieves CIPP logs matching "alert" on 20240711
 #>
 
 function Get-CIPPLogs {
     [CmdletBinding()]
-    Param()
+    Param(
+        $Severity,
+        $DateFilter
+    )
 
-    Write-Verbose "Getting CIPP Logs"
     $endpoint = "/api/ListLogs"
-    
-    Invoke-CIPPRestMethod -Endpoint $endpoint
+
+    if($Severity -or $DateFilter){
+        
+        $Params = @{
+            'Filter' = $True
+        }
+
+        if($Severity){
+            $Params['Severity'] = $Severity
+        }
+        
+        if($DateFilter){
+            $Params['DateFilter'] = $DateFilter
+        }else{
+            $Params['DateFilter'] = (Get-Date -Format "yyyyMMdd")
+        }
+
+        Write-Verbose "Getting CIPP Logs"
+        Invoke-CIPPRestMethod -Endpoint $endpoint -Param $Params
+
+    }else{
+
+        Write-Verbose "Getting CIPP Logs"
+        Invoke-CIPPRestMethod -Endpoint $endpoint
+
+    }
 }
