3373d764e8f6e499b964a78d5260120b6bad858a
BNWEIN <bweinberg@prime-networks.co.uk>
2025-01-19 17:03:45 +0000

    Refactor and update CIPPAPIModule: Removed Get-CIPPGDAPInvite function, updated module version to 1.2.0, and improved documentation for Add-CIPPRoomMailbox. Adjusted parameter handling in Get-CIPPGraphRequest for better clarity.

--- CIPPAPIModule/public/Tenant/GDAP/New-CIPPGDAPInvite.ps1
+++ CIPPAPIModule/public/Tenant/GDAP/New-CIPPGDAPInvite.ps1
@@ -3,7 +3,7 @@
 Creates a GDAP (Granular Delegated Admin Privileges) invite.
 
 .DESCRIPTION
-The Get-CIPPGDAPInvite function sends a request to create a GDAP invite using the specified GDAP roles. 
+The New-CIPPGDAPInvite function sends a request to create a GDAP invite using the specified GDAP roles. 
 You can either provide a custom set of roles using the `-GDAPRoles` parameter or include all existing roles by using the `-UseAllExistingRoles` switch.
 
 .PARAMETER GDAPRoles
@@ -17,12 +17,12 @@ An array of GDAP roles to be included in the invite. Each role is represented as
 A switch parameter that, when specified, includes all existing roles in the GDAP invite. This is mutually exclusive with `-GDAPRoles`.
 
 .EXAMPLE
-PS C:\> Get-CIPPGDAPInvite -GDAPRoles @(@{GroupName="M365 GDAP Cloud Device Administrator";GroupId="fa03defa-27c4-4639-8e50-14cbb746a78d";RoleName="Cloud Device Administrator";roleDefinitionId="7698a772-787b-4ac8-901f-60d6b08affd2"},@{GroupName="M365 GDAP Intune Administrator";GroupId="3d1c917f-8d1e-4a1e-a61c-df3263a0d1bc";RoleName="Intune Administrator";roleDefinitionId="3a2c62db-5318-420d-8d74-23affee5d9d5"})
+PS C:\> New-CIPPGDAPInvite -GDAPRoles @(@{GroupName="M365 GDAP Cloud Device Administrator";GroupId="fa03defa-27c4-4639-8e50-14cbb746a78d";RoleName="Cloud Device Administrator";roleDefinitionId="7698a772-787b-4ac8-901f-60d6b08affd2"},@{GroupName="M365 GDAP Intune Administrator";GroupId="3d1c917f-8d1e-4a1e-a61c-df3263a0d1bc";RoleName="Intune Administrator";roleDefinitionId="3a2c62db-5318-420d-8d74-23affee5d9d5"})
 
 This example creates a GDAP invite with the roles "Cloud Device Administrator" and "Intune Administrator."
 
 .EXAMPLE
-PS C:\> Get-CIPPGDAPInvite -UseAllExistingRoles
+PS C:\> New-CIPPGDAPInvite -UseAllExistingRoles
 
 This example creates a GDAP invite including all existing roles retrieved by the `Get-CIPPGDAPRoles` function.
 
@@ -32,7 +32,7 @@ This example creates a GDAP invite including all existing roles retrieved by the
 - Ensure the GDAP roles are valid and correctly formatted before calling this function.
 #>
 
-function Get-CIPPGDAPInvite {
+function New-CIPPGDAPInvite {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory = $false)]
@@ -59,8 +59,8 @@ function Get-CIPPGDAPInvite {
     $endpoint = '/api/ExecGDAPInvite'
 
     $Body = @{
-        gdapRoles = $GDAPRoles
+        roleMappings = $GDAPRoles
     }
 
-    Invoke-CIPPRestMethod -Endpoint $endpoint -Body $Body
+    Invoke-CIPPRestMethod -Endpoint $endpoint -Body $Body -Method Post
 }
