bcc93d63f7662faa6e4dba54f675ab07326a546e
Kristian Kjærgård <kris6673@hotmail.com>
2025-03-20 16:43:57 +0100

    Rename Set-CIPPClrImmID to Clear-CIPPImmutableID and enhance documentation for clarity

--- CIPPAPIModule/public/Identity/Administration/Users/Set-CIPPClrImmID.ps1
+++ CIPPAPIModule/public/Identity/Administration/Users/Set-CIPPClrImmID.ps1
@@ -1,37 +1,50 @@
 <#
 .SYNOPSIS
-Clears the Immutable ID for a user.
+Clears the Immutable ID for a user in a specified tenant.
 
 .DESCRIPTION
-The Set-CIPPClrImmID function clears the Immutable ID for a user by invoking a REST API endpoint.
+The Clear-CIPPImmutableID function removes the Immutable ID attribute from a user account in a specified tenant.
+This is commonly used when migrating users or fixing synchronization issues between on-premises Active Directory and Azure AD.
 
 .PARAMETER CustomerTenantID
-The ID of the customer tenant.
+The tenant ID of the customer where the user exists.
+This parameter is mandatory and should be in the format of a domain name (e.g., "contoso.onmicrosoft.com") or the tenant ID.
 
 .PARAMETER UserID
-The ID of the user.
+The ID of the user whose Immutable ID will be cleared. Can be either the user's UserPrincipalName (UPN) or their GUID.
+This parameter is mandatory.
 
 .EXAMPLE
-Set-CIPPClrImmID -CustomerTenantID "7ced1621-b8f7-4231-868c-bc6b1a2f1778" -UserID "281ceb6e-3d12-4a7f-b571-3c4f35ad85bc"
+Clear-CIPPImmutableID -CustomerTenantID "contoso.onmicrosoft.com" -UserID "user@contoso.com"
+Clears the Immutable ID for the specified user in the Contoso tenant.
 
-This example clears the Immutable ID for the user with the specified CustomerTenantID and UserID.
+.EXAMPLE
+Set-CIPPClrImmID -CustomerTenantID "fabrikam.onmicrosoft.com" -UserID "12345678-1234-1234-1234-123456789012"
+Uses the alias to clear the Immutable ID for the user with the specified GUID in the Fabrikam tenant.
 
+.NOTES
+This function calls the CIPP API endpoint '/api/ExecClrImmId' to perform the operation.
+Requires appropriate permissions to modify user attributes in the specified tenant.
+
+.LINK
+https://docs.cipp.app/
 #>
 
-function Set-CIPPClrImmID {
+function Clear-CIPPImmutableID {
     [CmdletBinding()]
+    [Alias('Set-CIPPClrImmID')]
     Param(
         [Parameter(Mandatory = $true)]
         [string]$CustomerTenantID,
         [Parameter(Mandatory = $true)]
-        [guid]$UserID
+        [string]$UserID
     )
 
-    Write-Verbose "Clearing Immutable ID for User: $userID"
+    Write-Verbose "Clearing Immutable ID for User: $UserID"
 
-    $endpoint = '/api/execclrimmid'
+    $endpoint = '/api/ExecClrImmId'
     $params = @{
-        tenantfilter = $CustomerTenantID
+        tenantFilter = $CustomerTenantID
         Id           = $UserID
     }
     Invoke-CIPPRestMethod -Endpoint $endpoint -Params $params
